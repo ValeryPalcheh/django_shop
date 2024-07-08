9be5a4568259f399f34836dc3d52bb5fdefa3af1
V <pvv.1983.minsk@gmail.com>
2024-07-08 12:09:21 +0300

    Изменение

--- src/shop_app/models.py
+++ src/shop_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# ORM
+# Reference_books
+# category | description
+
+class Reference_book(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
