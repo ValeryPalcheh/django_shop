4830a5d12bb4e641f3b2e2e9b793105231bc7e3d
V <pvv.1983.minsk@gmail.com>
2024-07-08 10:55:54 +0300

    Изменение

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/home/ValeryPalchekh/static'
+MEDIA_ROOT = '/home/ValeryPalchekh/media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
