62973aca51aaf9c74456b56dd643365405c29676
V <pvv.1983.minsk@gmail.com>
2024-07-10 13:57:21 +0300

    Изменение

--- src/shop_app/models.py
+++ src/shop_app/models.py
@@ -15,3 +15,19 @@ class Reference_book(models.Model):
     def __str__(self):
         return f"{self.name} # {self.pk}"
     
+
+class Book(models.Model):
+        # pk # number # reference book #
+    number = models.CharField(max_length=100)
+    reference_book = models.ForeignKey(
+        Reference_book,
+        on_delete=models.PROTECT,
+        related_name="books"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f"book # {self.pk}"
